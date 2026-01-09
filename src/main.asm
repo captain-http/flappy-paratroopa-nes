59361856da09141e1416f96a2e1ac47c5c59fadd
Luis Alfredo Lorenzo <babasbot@gmail.com>
2026-01-08 21:25:49 -0600

    feat: fill screen with light blue background ($21)
    Phase 1: Render static background
    - Proper PPU init (2 vblank waits)
    - Clear RAM during warmup
    - Load palette color $21 at $3F00
    - Enable background rendering

--- src/main.asm
+++ src/main.asm
@@ -31,8 +31,51 @@
 reset:
     sei                   ; Disable interrupts
     cld                   ; Clear decimal mode
+    ldx #$40
+    stx $4017             ; Disable APU frame IRQ
     ldx #$FF
     txs                   ; Initialize stack
+    inx                   ; X = 0
+    stx PPU_CTRL          ; Disable NMI
+    stx PPU_MASK          ; Disable rendering
+    stx $4010             ; Disable DMC IRQs
+
+    ; Wait for first vblank (PPU warmup)
+@vblank1:
+    bit PPU_STATUS
+    bpl @vblank1
+
+    ; Clear RAM while waiting for second vblank
+    lda #$00
+@clear_ram:
+    sta $0000, x
+    sta $0100, x
+    sta $0200, x
+    sta $0300, x
+    sta $0400, x
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+    inx
+    bne @clear_ram
+
+    ; Wait for second vblank (PPU ready)
+@vblank2:
+    bit PPU_STATUS
+    bpl @vblank2
+
+    ; Load background color into palette
+    bit PPU_STATUS        ; Reset PPU address latch
+    lda #$3F
+    sta PPU_ADDR
+    lda #$00
+    sta PPU_ADDR          ; PPU address = $3F00
+    lda #$21              ; Light blue
+    sta PPU_DATA
+
+    ; Enable rendering
+    lda #%00001000        ; Show background
+    sta PPU_MASK
 
 forever:
     jmp forever           ; Infinite loop
